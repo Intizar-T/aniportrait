199b2a473c2e08ddbedb28f662958e4f202f1239
Intizar <intizar.tashov22@gmail.com>
2024-04-29 13:55:31 +0900

    add example input

--- input_schema.py
+++ input_schema.py
@@ -3,10 +3,12 @@ INPUT_SCHEMA = {
         "datatype": "STRING",
         "required": True,
         "shape": [1],
+        "example": ["https://nocodingai-backend.s3.amazonaws.com/f30d1406-fde4-4a3d-8c17-69aa8c8e920e/5e02ec5e-2db3-4644-98e0-ce50e7884921.png"]
     },
     "video_url": {
         "datatype": "STRING",
         "required": True,
         "shape": [1],
+        "example": ["https://intizar-bucket.s3.ap-northeast-2.amazonaws.com/15261364-89c8-43af-b3a2-80fe0a25ec8d.mp4"]
     },
 }
