aa08404f3e7143a0db3ae7325dc25def87403cba
Intizar <intizar.tashov22@gmail.com>
2024-04-29 19:21:09 +0900

    remove subprocess and move infer script to app

--- scripts/audio2vid.py
+++ scripts/audio2vid.py
@@ -261,8 +261,7 @@ def main():
             
             stream = ffmpeg.input(save_path)
             audio = ffmpeg.input(audio_path)
-            video_path = f"{audio_path.split('.')[0]}.mp4"
-            ffmpeg.output(stream.video, audio.audio, video_path, vcodec='copy', acodec='aac', shortest=None).run()
+            ffmpeg.output(stream.video, audio.audio, save_path.replace('_noaudio.mp4', '.mp4'), vcodec='copy', acodec='aac', shortest=None).run()
             os.remove(save_path)
 
 if __name__ == "__main__":
